fee1bbfd05463dba687aa1335ee518e763609cdb
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2022-05-10 09:56:19 +0200

    cors disable

--- src/main/java/com/grupo/apirest/security/SecurityConfig.java
+++ src/main/java/com/grupo/apirest/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
+        http.cors().disable().csrf().disable().authorizeRequests()
                 //.mvcMatchers("/api/public").permitAll()
                 .mvcMatchers("/powerlifting/**").authenticated()
                 .mvcMatchers("/private-scoped/**").hasAuthority("SCOPE_superadmin")
