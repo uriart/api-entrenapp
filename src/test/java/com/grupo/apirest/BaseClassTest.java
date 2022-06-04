857b63fd6b817ecc6957b32d7cd482ad89db1eb4
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2022-06-04 18:28:25 +0200

    master: tests

---
+++ src/test/java/com/grupo/apirest/BaseClassTest.java
@@ -0,0 +1,62 @@
+package com.grupo.apirest;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.grupo.apirest.controller.PowerProgramRestController;
+import com.grupo.apirest.dao.IPowerProgramDAO;
+import com.grupo.apirest.entity.Marcas;
+import com.grupo.apirest.service.IPowerProgramService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+public class BaseClassTest {
+
+    @Autowired
+    private IPowerProgramService powerProgramService;
+
+    public MockMvc mvc;
+
+    public String mapToJson(Object object) {
+        String json = "";
+        ObjectMapper ow = new ObjectMapper();
+        ow.setTimeZone(TimeZone.getTimeZone("Europe/Madrid"));
+        ow.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        try {
+            json = ow.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        PowerProgramRestController powerProgramRestController = new PowerProgramRestController();
+        ReflectionTestUtils.setField(powerProgramRestController, "powerProgramService", powerProgramService);
+        mvc = MockMvcBuilders.standaloneSetup(powerProgramRestController).build();
+    }
+
+    protected Marcas initMarcas(){
+        Marcas marcas = new Marcas();
+        marcas.setCodigoEspalda1("A");
+        marcas.setCodigoEspalda2("B");
+        marcas.setCodigoHombro("C");
+        marcas.setFechaInicio(new Date());
+        marcas.setPesoMuerto(1);
+        marcas.setPressBanca(1);
+        marcas.setSentadilla(1);
+        marcas.setUsuario("user1");
+        return marcas;
+    }
+}
---
+++ src/test/java/com/grupo/apirest/controller/PowerProgramRestControllerTest.java
@@ -0,0 +1,140 @@
+package com.grupo.apirest.controller;
+
+import com.grupo.apirest.BaseClassTest;
+import com.grupo.apirest.dao.IPowerProgramDAO;
+import com.grupo.apirest.entity.Marcas;
+import com.grupo.apirest.entity.Nota;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PowerProgramRestControllerTest extends BaseClassTest {
+
+    @MockBean
+    private IPowerProgramDAO powerProgramDAO;
+    private final String USUARIO = "user1";
+    private final Long ID_NOTA = new Long(1);
+    @Test
+    void saveMarcas() {
+        when(powerProgramDAO.saveMarcas(any(Marcas.class))).thenReturn(initMarcas());
+        String url = "/powerlifting/marcas";
+
+        try {
+            mvc.perform(MockMvcRequestBuilders.post(url)
+                .content(mapToJson(initMarcas()))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void getMarcas() {
+        when(powerProgramDAO.findByUser(eq(USUARIO))).thenReturn(initMarcas());
+        String url = "/powerlifting/marcas";
+
+        try {
+            mvc.perform(MockMvcRequestBuilders.get(url)
+                            .param("user", USUARIO)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void getProgram() {
+        when(powerProgramDAO.findByUser(eq(USUARIO))).thenReturn(initMarcas());
+        String url = "/powerlifting/program";
+
+        try {
+            mvc.perform(MockMvcRequestBuilders.get(url)
+                            .param("user", USUARIO)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            fail();
+        }
+
+    }
+
+    @Test
+    void getNotesByUser() {
+        when(powerProgramDAO.getNotesByUser(eq(USUARIO))).thenReturn( new ArrayList<>());
+        String url = "/powerlifting/getNotesByUser";
+
+        try {
+            mvc.perform(MockMvcRequestBuilders.get(url)
+                            .param("user", USUARIO)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void getNoteById() {
+        when(powerProgramDAO.getNoteById(eq(ID_NOTA))).thenReturn( null );
+        String url = "/powerlifting/getNoteById";
+
+        try {
+            mvc.perform(MockMvcRequestBuilders.get(url)
+                            .param("id", ID_NOTA.toString())
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void saveNota() {
+        String url = "/powerlifting/saveNota";
+
+        try {
+            mvc.perform(MockMvcRequestBuilders.post(url)
+                            .content(mapToJson(new Nota()))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void deleteNota() {
+        String url = "/powerlifting/deleteNota";
+
+        try {
+            mvc.perform(MockMvcRequestBuilders.delete(url)
+                            .param("id", ID_NOTA.toString())
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+
+
+}
